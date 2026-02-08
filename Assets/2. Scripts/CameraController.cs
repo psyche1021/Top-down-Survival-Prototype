7fb1c7f94f317c58c4172f0230f31f5d0bbd3c45
KwonYJ <66343178+psyche1021@users.noreply.github.com>
2026-02-08 22:47:37 +0900

    feat: Add camera and character movement system
    - Character movement
     - Implement player movement input  handling
    - Camera system:
     - Follow mode
     - Edge scroll

--- Assets/2. Scripts/CameraController.cs
+++ Assets/2. Scripts/CameraController.cs
@@ -2,13 +2,86 @@
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField, Tooltip("카메라 이동속도")] float moveSpeed = 50f;
+    [SerializeField, Tooltip("가장자리 경계선 크기")] float borderSize = 5f;
+
+    // 나중에 맵 크기에 따라 변경할 것
+    [SerializeField, Tooltip("최소 이동 가능 범위")] Vector2 minLimit;
+    [SerializeField, Tooltip("최소 이동 가능 범위")] Vector2 maxLimit;
+
+    [SerializeField, Tooltip("따라다닐 대상")] Transform target;
+
+    Vector3 targetPos; // 카메라 위치
+    bool cameraToggle = false;
+    bool isFollowing = false;
+
     void Start()
     {
-        
+        if (target == null)
+        {
+            Debug.LogError("CameraController에 Target이 없음");
+            enabled = false;
+            return;
+        }
+
+        targetPos = transform.position;
     }
 
     void Update()
     {
+        CameraToggle();
 
+        if (isFollowing)
+        {
+            CameraMove();
+        }
+        else
+        {
+            Vector3 dir = GetEdgeDirection();
+            CameraDirMove(dir);
+            ClampPosition();
+        }
+
+        transform.position = targetPos;
+    }
+
+    void CameraDirMove(Vector3 dir)
+    {
+        if (dir == Vector3.zero) return;
+
+        targetPos += dir * moveSpeed * Time.deltaTime;
+    }
+
+    void ClampPosition()
+    {
+        targetPos.x = Mathf.Clamp(targetPos.x, minLimit.x, maxLimit.x);
+        targetPos.z = Mathf.Clamp(targetPos.z, minLimit.y, maxLimit.y);
+    }
+
+    void CameraToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            cameraToggle = !cameraToggle;
+        }
+
+        isFollowing = Input.GetKey(KeyCode.Space) || cameraToggle;
+    }
+
+    void CameraMove()
+    {
+        targetPos = target.position;
+    }
+
+    Vector3 GetEdgeDirection()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 dir = Vector3.zero;
+
+        if (mouse.y >= Screen.height - borderSize) dir.z += 1; // 상
+        if (mouse.y <= borderSize) dir.z -= 1; // 하
+        if (mouse.x <= borderSize) dir.x -= 1; // 좌
+        if (mouse.x >= Screen.width - borderSize) dir.x += 1; // 우
+        return dir.normalized;// 대각선 이동 보간
     }
 }
--- Assets/2. Scripts/PlayerInput.cs
+++ Assets/2. Scripts/PlayerInput.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 public class PlayerInput : MonoBehaviour
 {
     Camera mainCamera;
     Character character;
+
+    [SerializeField] GameObject clickEffect;
 
     void Awake()
     {
@@ -17,9 +18,15 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetMouseButton(1))
         {
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 character.Movement.MoveTo(hit.point);
+
+                if (Input.GetMouseButtonDown(1))
+                {
+                    Instantiate(clickEffect, hit.point + Vector3.up * 0.03f, Quaternion.LookRotation(hit.normal));
+                }
             }
         }
 
@@ -30,4 +37,4 @@ public class PlayerInput : MonoBehaviour
     }
 }
 
-// 딕셔너리를 활용해 키 입력과 행동을 매핑해 확장성을 고려하는 방안은 추후 버튼이 많아지면 고려
+// 추후 버튼이 많아지면 딕셔너리를 활용하여 키 입력과 행동을 매핑해 확장성을 고려
